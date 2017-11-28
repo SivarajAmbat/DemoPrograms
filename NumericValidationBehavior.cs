df4a1e6d1cdebc77d1fcbdc23296781fbe1109a2
Sivaraj Ambat <SivarajTechM@users.noreply.github.com>
2017-11-28 19:57:23 +0530

    Attached Behavior Demo for Xamarin

---
+++ NumericValidationBehavior.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AttachedBehaviorDemo
+{
+    public static class NumericValidationBehavior
+    {
+        public static readonly BindableProperty AttachBehaviorProperty =
+            BindableProperty.CreateAttached("AttachBehavior",
+                typeof(bool),
+                typeof(NumericValidationBehavior),
+                false,
+                propertyChanged: OnAttachBehaviorChanged);
+
+        public static bool GetAttachBehavior(BindableObject view)
+        {
+            return (bool)view.GetValue(AttachBehaviorProperty);
+        }
+
+        public static void SetAttachBehavior(BindableObject view, bool value)
+        {
+            view.SetValue(AttachBehaviorProperty, value);
+        }
+
+
+        static void OnAttachBehaviorChanged(BindableObject view, object oldValue, object newValue)
+        {
+            var entry = view as Entry;
+            if (entry == null)
+            {
+                return;
+            }
+
+            bool attachBehavior = (bool)newValue;
+            if (attachBehavior)
+            {
+                entry.TextChanged += Entry_TextChanged;
+            }
+            else
+            {
+                entry.TextChanged -= Entry_TextChanged;
+            }
+
+        }
+
+        private static void Entry_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            double result;
+            bool isValid = double.TryParse(e.NewTextValue, out result);
+            ((Entry)sender).TextColor = isValid ? Color.Default : Color.Red;
+        }
+    }
+}
